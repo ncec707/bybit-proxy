5afd61d214e8037b39886fa93603921102be67ab
ncec707 <necipcancagman@gmail.com>
2025-06-08 16:56:21 +0300

    Update funding.js

--- api/funding.js
+++ api/funding.js
@@ -1,18 +1,16 @@
 export default async function handler(req, res) {
-  const url = "https://api.bybit.com/v5/market/funding/prev-funding-rate?symbol=BTCUSDT";
+  const url = "https://api.bybit.com/v2/public/tickers?symbol=BTCUSDT";
 
   try {
     const response = await fetch(url);
     const result = await response.json();
 
-    const data = {
-      symbol: result.result.symbol,
-      fundingRate: result.result.fundingRate,
-      fundingTime: result.result.fundingRateTimestamp
-    };
-
-    res.status(200).json(data);
+    res.status(200).json({
+      symbol: result.result[0].symbol,
+      last_price: result.result[0].last_price,
+      price_24h_pcnt: result.result[0].price_24h_pcnt
+    });
   } catch (error) {
-    res.status(500).json({ error: "Bybit API request failed" });
+    res.status(500).json({ error: "Bybit TEST API request failed" });
   }
 }
